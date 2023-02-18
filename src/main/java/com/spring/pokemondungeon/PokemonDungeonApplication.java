7b34421e3cb6e03c0096a37bfc714e94b0728677
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-02-18 19:55:02 +0900

    update : servlet annotation

--- src/main/java/com/spring/pokemondungeon/PokemonDungeonApplication.java
+++ src/main/java/com/spring/pokemondungeon/PokemonDungeonApplication.java
@@ -2,7 +2,9 @@ package com.spring.pokemondungeon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class PokemonDungeonApplication {
 
